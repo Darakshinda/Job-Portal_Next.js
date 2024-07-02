0154537fad1367cc8f7b4efe83aaedb73968ab39
Darakshinda <2021csb1130@iitrpr.ac.in>
2024-07-02 16:56:07 +0530

    update tags

--- src/stories/Tags2.tsx
+++ src/stories/Tags2.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Tag from '@/app/Components/Tag';
 import ClickOutsideDiv from '../app/Components/ClickoutsideDiv';
-import "@/app/Components/Clickoutsidediv.css";
+import "../app/Components/ClickoutsideDiv.css";
 
 
 interface Option {
