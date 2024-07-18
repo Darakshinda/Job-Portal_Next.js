75f25d7dcf03a113270a65ffeb8057316ffa1416
Rahul Kurup <rpsg1819@gmail.com>
2024-07-19 00:01:33 +0530

    Applypop close on outside click

--- src/app/Components/ApplyPopup.tsx
+++ src/app/Components/ApplyPopup.tsx
@@ -51,10 +51,10 @@ const ApplyPopup = ({ job, onClose }) => {
   return (
     
     <div className="fixed inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)]">
-      <ClickOutsideDiv cls="bg-[#2331b3] p-6 rounded-lg shadow-lg w-96 text-white" onOutsideClick={onClose}>
+      <ClickOutsideDiv cls="bg-[#3c3d71] p-6 rounded-lg shadow-lg w-96 text-white" onOutsideClick={onClose}>
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-xl font-bold">Apply for {job.position} position</h2>
-          <button className='text-red-700' onClick={onClose}>
+          <button className='text-[#ff1717]' onClick={onClose}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
@@ -74,12 +74,12 @@ const ApplyPopup = ({ job, onClose }) => {
             className={`mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm ${resumeError ? 'border-red-500' : ''}`}
             required
           />
-          {resumeError && <p className="text-red-700 text-sm mt-1">Please upload your resume.</p>}
+          {resumeError && <p className="text-[#ff005f] text-sm mt-1">Please upload your resume.</p>}
         </div>
         <div className="mb-4">
           <label className="block">Cover Letter</label>
           <textarea
-            className="w-full border border-gray-300 rounded-md p-2 text-sm focus:ring-blue-500 focus:border-blue-500"
+            className="placeholder-white w-full bg-[#a3a3a3] border border-gray-300 rounded-md p-2 text-sm focus:ring-blue-500 focus:border-blue-500"
             rows="4"
             value={coverLetter}
             onChange={handleCoverLetterChange}
@@ -87,7 +87,7 @@ const ApplyPopup = ({ job, onClose }) => {
           />
         </div>
         <button
-          className="bg-purple-500 text-white py-2 px-4 rounded-md"
+          className="bg-[#ff005f] text-white py-2 px-4 rounded-md"
           onClick={handleSubmit}
         >
           Submit
